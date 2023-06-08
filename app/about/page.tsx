76967462b4b7fba2706be6fb146728d724fe3403
Haesoo Jeong <wjdgotn77@gmail.com>
2023-06-08 13:06:48 +0700

    add : about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+
 export default function AboutPage() {
-  return <>About</>;
+  return (
+    <>
+      <Image src="/about.jpeg" width="200" height="200" alt="avatar" />
+      <div className="whitespace-pre-wrap">
+        {`I am a first-year front-end developer working for a company that provides a nutritional subscription service.\nThis blog was created to post and archive development-related articles of interest.`}
+      </div>
+    </>
+  );
 }
