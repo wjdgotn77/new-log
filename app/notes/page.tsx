e24287e96ef51d1fda798a88d0b9e844a248623c
Haesoo Jeong <wjdgotn77@gmail.com>
2023-06-08 12:44:32 +0700

    add : note page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,33 @@
-import SearchBar from "@/components/SearchBar";
+import Link from "next/link";
 
-export default function NotesPage() {
+import { compareDesc, format, parseISO } from "date-fns";
+import { allNotes, type Notes } from "contentlayer/generated";
+
+export default function BlogPage() {
+  const notes = allNotes.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
+
   return (
-    <>
-      Notes
-      <SearchBar />
-    </>
+    <div className="flex flex-col">
+      <div className="pt-[20px]">
+        {notes.map((post, idx) => (
+          <PostCard key={idx} {...post} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function PostCard(note: Notes) {
+  return (
+    <div className="flex items-center">
+      <Link href={note.url} className="pr-[10px]">
+        {note.title}
+      </Link>
+      <time dateTime={note.date} className="text-sm text-gray-600">
+        {format(parseISO(note.date), "LLLL d, yyyy")}
+      </time>
+    </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,6 @@ export default function NavBar() {
   return (
     <div className="flex gap-2 font-semibold pb-[20px]">
       <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
       <Link href="/notes">Notes</Link>
       <Link href="/about">About</Link>
     </div>
